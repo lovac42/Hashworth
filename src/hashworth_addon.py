967f611ed2b7db3252f62e3899730b671a649884
lovac42 <lovac42@protonmail.com>
2020-03-07 15:32:37 -0500

    Updated copyright to 2020

--- src/hashworth_addon.py
+++ src/hashworth_addon.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright: (C) 2019 Lovac42
-# Support: https://github.com/lovac42/Wordsworth
+# Copyright: (C) 2019-2020 Lovac42
+# Support: https://github.com/lovac42/Hashworth
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
 
